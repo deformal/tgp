77b7298da28362410b7b6d4ce31858b3daa0cc5e
deformal <drake9211@gmail.com>
2021-10-31 13:42:52 +0530

    inittial

---
+++ src/bin/cli.ts
@@ -0,0 +1,25 @@
+#!/usr/bin/env node
+
+import { execSync } from 'child_process';
+
+const runCommand = (command: string) => {
+  try {
+    execSync(`${command}`, { stdio: 'inherit' });
+  } catch (err) {
+    console.error(`Failed to execute ${command}`, err);
+    return false;
+  }
+  return true;
+};
+const reppoName = process.argv[2];
+const gitCheckOut = `git clone  https://github.com/deformal/type-graphql-prisma ${reppoName}`;
+const installDepsCommand = `cd${reppoName} $$ npm install`;
+console.log(`Cloning the repository with name ${reppoName}`);
+const checkOut = runCommand(gitCheckOut);
+if (!checkOut) process.exit(-1);
+
+console.log(`Installing dependencies for ${reppoName}}`);
+const installedDeps = runCommand(installDepsCommand);
+if (!installedDeps) process.exit(-1);
+console.log('Thank you for using tgp, template. Happy coding 🚀');
+console.log(`cd ${reppoName} && yarn or npm install`);
